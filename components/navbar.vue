1f630112a1a2be0f157e0e36e938c8e250235f8d
Brojaga <lawnj00@gmail.com>
2020-12-07 14:39:02 -0600

    Test for Jacob

---
+++ components/navbar.vue
@@ -0,0 +1,33 @@
+  <template>
+    <div class="center examplex">
+      <vs-navbar color="#7d33ff" text-white square center-collapsed v-model="active">
+        <template #left>
+        <vs-navbar-item :active="active == 'matchup'" id="matchup">
+          Matchup
+        </vs-navbar-item>
+        <vs-navbar-item :active="active == 'rankings'" id="rankings">
+          <a href="/rankingsPage">Rankings</a>
+        </vs-navbar-item>
+        <vs-navbar-item :active="active == 'about'" id="about">
+          About
+        </vs-navbar-item>
+        </template>
+      </vs-navbar>
+    </div>
+  </template>
+
+
+<script>
+export default {
+    data:() => ({
+      active: 'matchup'
+    })
+}
+</script>
+
+<style>
+a {
+  text-decoration: none;
+  color: white;
+}
+</style>
---
+++ pages/rankingsPage.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <navbar />
+    <h1>
+      HELLO THIS IS THE RANKINGS PAGE
+    </h1>
+  </div>
+</template>
+
+
+<script>
+import navbar from '~/components/navbar.vue'
+export default {
+  components: {
+    navbar
+  }
+}
+</script>
+
+<style>
+
+</style>
